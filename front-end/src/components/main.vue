2dee024d1d39f40e712f201c6cde96664356d6fa
wangjunfei <471612627@qq.com>
2024-01-26 00:59:35 +0800

    加入了后端文件夹，同时进行前端请求的配置

--- front-end/src/components/main.vue
+++ front-end/src/components/main.vue
@@ -4,10 +4,16 @@
 
 <script setup>
 import router from "@/router";
-
+import request from "@/utils/request";
+import {onMounted} from "vue";
 const beginAssess = () => {
   router.push('/main')
 }
+onMounted(()=>{
+  request.get('').then((json)=>{
+    console.log(json)
+  })
+})
 </script>
 
 
