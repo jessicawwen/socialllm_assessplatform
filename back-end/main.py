ab772aecdcf6a23168571d5f036f29fa4ba681e6
wangjunfei <471612627@qq.com>
2024-01-26 01:00:25 +0800

    加入了后端文件夹

---
+++ back-end/main.py
@@ -0,0 +1,21 @@
+from typing import Union
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有头
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
